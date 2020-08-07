e8bb6687d188555051f72a2efc7e2287850da8f2
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-06 21:28:43 -0400

    added inserts to seed file and commented out of schema file

--- schema.sql
+++ schema.sql
@@ -68,6 +68,6 @@ SELECT * FROM departments;
 SELECT * FROM role;
 SELECT * FROM employee;
 
-INSERT INTO departments(name) VALUES("Front-End Developer");
-INSERT INTO role(title, salary, department_id) VALUES("TEST", 80000.00, 123);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES("Xavier", "Jackson", 629, 7039);
+-- INSERT INTO departments(name) VALUES("Front-End Developer");
+-- INSERT INTO role(title, salary, department_id) VALUES("TEST", 80000.00, 123);
+-- INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES("Xavier", "Jackson", 629, 7039);
---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO departments(name) VALUES("Front-End Developer");
+INSERT INTO role(title, salary, department_id) VALUES("TEST", 80000.00, 123);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES("Xavier", "Jackson", 629, 7039);
