a1ded1be74f4d2ecd3f9622fc43abb2695a270d2
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-06 20:46:35 -0400

    added connection

--- employeeTrackerConnectionDB.js
+++ employeeTrackerConnectionDB.js
@@ -14,6 +14,25 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "xej62920",
+    database: "employee_trackerDB",
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
+
 
 
 
